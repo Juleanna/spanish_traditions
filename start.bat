b7e988fdc2256bcad7dd8212b74d5ad13f1a7b34
Juleanna <juleannadafna@gmail.com>
2025-05-22 16:04:32 +0300

    fix

--- start.bat
+++ start.bat
@@ -1,6 +1,14 @@
 @echo off
-:: Укажите полный путь к виртуальному окружению
-call C:\venv\Scripts\activate
+:: Переход в директорию проекта
+cd /d C:\spanish_traditions
+
+:: Активация виртуального окружения
+call C:\venv\Scripts\activate.bat
+
+:: Проверка версии Python и установленных модулей
+python --version
+where python
+pip show django-ckeditor-5
 
 :: Запуск сервера Django
 python manage.py runserver
