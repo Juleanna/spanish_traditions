094130d5ab7aa363d961b5cb1ee753b761c2b455
Juleanna <juleannadafna@gmail.com>
2025-05-23 16:50:25 +0300

    menu

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,7 +1,24 @@
 from .models import MenuItem
 
+def build_menu_tree(parent):
+    """
+    Рекурсивно строит дерево меню, добавляя дочерние элементы.
+    """
+    children = parent.children.filter(is_active=True).order_by('menu_order')
+    return [
+        {'item': child, 'children': build_menu_tree(child)}
+        for child in children
+    ]
+
 def menu_context(request):
-    menu_items = MenuItem.objects.filter(parent__isnull=True, is_active=True).order_by('menu_order')
+    """
+    Формирует контекст для меню, добавляя дерево пунктов меню.
+    """
+    root_items = MenuItem.objects.filter(parent__isnull=True, is_active=True).order_by('menu_order')
+    menu_tree = [
+        {'item': item, 'children': build_menu_tree(item)}
+        for item in root_items
+    ]
     return {
-        'menu_items': menu_items
+        'menu_tree': menu_tree
     }
